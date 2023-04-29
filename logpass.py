cef618176cb746c5d2f7154e050600ea63f5ff1d
artkotenko <132163249+artkotenko@users.noreply.github.com>
2023-04-29 22:35:30 +0300

    Create logpass.py

---
+++ logpass.py
@@ -0,0 +1,12 @@
+l = (input("Введите логин"))
+p = (input("Введите пароль"))
+login = "re.com" 
+password = "1234"
+if l == login:
+  print("Логин правильный")
+else:
+  print("Логин не верный ")
+if p == password:
+  print("Пароль правильный")
+else:
+  print("Пароль не верный")
